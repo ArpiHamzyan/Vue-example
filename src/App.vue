c3ed2b2b715f0a734dcf99c4854c387406031004
Arpi Hamzyan <arpihamzyan@MacBook-Air-Arpi.local>
2025-01-15 12:17:41 +0400

    Vue Forms

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,150 @@
 <template>
-<h2 v-for="(name,index) in names" :key="name">{{ index }}  {{ name }}</h2>
-<h2 v-for="name in fullNames" :key="name.first">{{ name.first }}  {{ name.last }}</h2>
-<div v-for="actor in actors" :key="actor.name">
-  <h2>{{ actor.name }}</h2>
-  <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-</div>
-<h2 v-for="(value,key,index) in myInfo" :key="value">{{index}} {{key}}  {{ value }}</h2>
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues,null,2) }}
+    </pre>
+  </div>
+  <form @submit.prevent="submitForm">
+      <div>
+        <label for="name">Vishwas</label>
+        <input type="text" id="name" v-model.trim.lazy="formValues.name">
+      </div>
+
+      <div>
+        <label for="profile">Profile Summary</label>
+        <textarea id="profile" v-model="formValues.profileSummary"></textarea>
+      </div>
+
+      <div>
+        <label for="country">Country</label>
+        <select  id="country" v-model="formValues.country">
+            <option value="">Select a country</option>
+            <option value="india">India</option>
+            <option value="vietnam">Vietnam</option>
+            <option value="singapore">Singapore</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="job-location">Job Location</label>
+        <select  id="job-location" multiple v-model="formValues.jobLocation">
+          <option value="india">India</option>
+          <option value="vietnam">Vietnam</option>
+          <option value="singapore">Singapore</option>
+        </select>
+      </div>
+
+      <div>
+        <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" 
+        true-value="yes"
+        false-value="no">
+        <label for="remoteWork">Open to remote work?</label>
+      </div>
+
+      <div>
+        <label for="">Skill Set</label>
+        <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
+        <label for="html">HTML</label>
+        <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
+        <label for="css">CSS</label>
+        <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet">
+        <label for="javascript">JavaScript</label>
+      </div>
+
+      <div>
+        <label >Yeasr of Experience</label>
+        <input 
+          type="radio"
+          id="0-2"
+          value="0-2"
+          v-model="formValues.yearsOfExperience"
+        >
+        <label for="0-2">0-2</label>
+        <input 
+          type="radio"
+          id="3-5"
+          value="3-5"
+          v-model="formValues.yearsOfExperience"
+        >
+        <label for="3-5">3-5</label>
+        <input 
+          type="radio"
+          id="6-10"
+          value="6-10"
+          v-model="formValues.yearsOfExperience"
+        >
+        <label for="6-10">6-10</label>
+        <input 
+          type="radio"
+          id="10+"
+          value="10+"
+          v-model="formValues.yearsOfExperience"
+        >
+        <label for="10+">10+</label>
+      </div>
+
+      <div>
+        <label for="age">Age</label>
+        <input  type="number" id="age" v-model="formValues.age">
+      </div>
+
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+  </form>
 </template>
-  
+
 <script>
-  export default {
-    name: "App",
-    data () {
-      return {
-        names:['Bruce','Clark','Diana'],
-        fullNames : [
-          { first: 'Bruce', last : 'Wayne'},
-          { first: 'Clark', last : 'Kent'},
-          { first: 'Princess', last : 'Diana'},
-        ],
-        actors : [
-          {
-            name : 'Christian Bale',
-            movies : ['Batman' , 'The Prestige'],
-          },
-          {
-            name : 'Di Caprio',
-            movies : ['Titanic' , 'Inception'],
-          },
-        ],
-        myInfo : {
-          name : 'Vishwas',
-          channel : 'chsd',
-          cours : 'Vue 3',
-        }
+export default {
+  name: 'App',
+  data() {
+    return {
+      formValues : {
+        name : '',
+        profileSummary : '',
+        country : '',
+        jobLocation : [],
+        remoteWork : 'no',
+        skillSet : [] ,
+        yearsOfExperience : '',
+        age : null,   
       }
     }
-  };
+  },
+  methods: {
+   submitForm () {
+    console.log('Form values',this.formValues)
+   }
+  },
+}
 </script>
-  
-  <style scoped>
-  
-  </style>
-  
+
+<style scoped>
+label{
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input [type = 'text'],
+textarea,
+select{
+   display: block;
+   width: 400px;
+   padding: 6px 12px;
+   font-size: 14px;
+   line-height: 1.42857143;
+   color: #555;
+   background-color: #fff;
+   background-image: none;
+   border: 1px solid #ccc;
+   border-radius: 4px;
+}
+
+</style>
