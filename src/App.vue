4c1299b199281192a343e562a5b2dc5d639f05c7
Arpi Hamzyan <arpihamzyan@MacBook-Air-Arpi.local>
2025-01-17 10:52:50 +0400

    props

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,29 @@
 <template>
- <h2>Volume Tracker (0-20)</h2>
- <h3>Current Volume   {{ volume }}</h3>
+ <!-- <Greet name="Bruce" heroName="Batman"/>
+ <Greet name="Clark" heroName="Superman"/>
+ <Greet name="Diana" heroName="Wonde Women"/>
+ <Greet :name="name" :heroName="channel"/> -->
 
- <div>
-  <button @click="volume += 2">Increment</button>
-  <button @click="volume -= 2">Decrement</button>
- </div>
-
- <input type="text" v-model="movie">
- <input type="text" v-model="movieInfo.title">
- <input type="text" v-model="movieInfo.actor">
- <div>
-  <button @click="movieList = movieList.concat(['Wonder Woman'])">Add Movie</button>
- </div>
+ <Article id="my-article" title="Article Title" :likes="50" :isPublished="true"/>
 </template>
 
 <script>
+//import Greet from './components/Greet.vue'
+import Article from './components/Article.vue'
+
 export default {
   name: 'App',
-  data() {
+  components : {
+    //Greet,
+    Article,
+  },
+  data () {
     return {
-      volume : 0,
-      movie : 'Batman',
-      movieInfo : {
-        title : '',
-        actor : ''
-      },
-      movieList : ['Batman' , 'Superman']
-    }
-  },
-  methods: {
-   
-  },
-  computed : {
-   
-  },
-  watch : {
-    volume (newValue,oldValue) {
-      if(newValue > oldValue && newValue === 16 ){
-        alert('Listening')
-      }
-    },
-    movie : {
-      handler(newValue) {
-        console.log(`Calling ${newValue}`)
-      },
-      immediate : true
-    },
-    movieInfo : {
-      handler (newValue) {
-        console.log(
-          `Calling movie title ${newValue.title} and actor ${newValue.actor}`
-        )
-      },
-      deep: true
-    },
-    movieList : {
-      handler (newValue) {
-        console.log(`Updated list ${newValue}`)
-      },
-      //deep:true
+      name: 'Vishwas',
+      channel: 'Codevolution'
     }
   }
+  
 }
 </script>
 
