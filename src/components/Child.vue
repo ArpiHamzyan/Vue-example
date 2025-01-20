98803ffc5bcc8813465b7d2bee654f09ea191df2
Arpi Hamzyan <arpihamzyan@MacBook-Air-Arpi.local>
2025-01-20 11:28:36 +0400

    lifecycle

---
+++ src/components/Child.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>Child Component</div>
+</template>
+    
+<script>
+ export default {
+      name: 'Child',
+      beforeCreate() {
+        console.log('Childe beforeCreate()')
+      },
+      created () {
+        console.log('Childe created ()')
+      },
+      beforeMount() {
+        console.log('Childe beforeMount()')
+      },
+      mounted(){
+        console.log('Childe mounted()')
+      },
+      beforeUpdate() {
+        console.log('Childe beforeUpdate()')
+      },
+      updated() {
+        console.log('Childe updated()')
+      },
+      beforeUnmount(){
+        console.log('Childe beforeUnmount()')
+      },
+      unmounted(){
+        console.log('Childe unmounted()')
+      },
+
+};
+</script>
+    
+<style scoped>
+    
+</style>
+    
